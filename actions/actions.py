047296bc84f708d8ee061ec704c6026c78b73355
Sneha <104077029+Sneha12c@users.noreply.github.com>
2023-04-10 23:01:18 +0530

    Add files via upload

--- actions/actions.py
+++ actions/actions.py
@@ -10,22 +10,18 @@
 from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker
-from rasa_sdk.executor import CollectingDispatcher
-from mysql_connectivity import insertquery
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from rasa_sdk.events import SlotSet, EventType
+from rasa_sdk.executor import CollectingDispatcher, Action
+import webbrowser
+from rasa_sdk.interfaces import Action
+from rasa_sdk.events import SlotSet ,EventType
+from rasa_sdk.forms import FormValidationAction
+from rasa_sdk.events import AllSlotsReset, SlotSet
+from rasa_sdk.events import UserUtteranceReverted
+from rasa_sdk.executor import CollectingDispatcher , Action
+from rasa_sdk.types import DomainDict
+
+
 class ActionTellName(Action):
 
     def name(self) -> Text:
@@ -51,24 +47,27 @@ class ActionTellName(Action):
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         
         number = tracker.get_slot("number")
-        message = " Your mobile number is {} ".format(number)
+      #  - text : " Hi {name}! , what is your mobile number 📱 ? "
+        message = " Yeah , Your mobile number   is {} . Thanks for giving information ".format(number)
         print (message)
         dispatcher.utter_message(text=message)
         return []
-    
-class ActionVideo(Action):
-    def name(self) -> Text:
-        return "action_query"
 
-    async def run(
-        self,
-        dispatcher,
-        tracker: Tracker,
-        domain: "DomainDict",
-    ) -> List[Dict[Text, Any]]:
-        dispatcher.utter_message("storing details for future evaluation.")
-        insertquery(tracker.get_slot("query"),tracker.get_slot("username"),tracker.get_slot("usermail"))
 
-        return []
+class ValidateHealthForm(FormValidationAction):
 
-        
+  def name(self) -> Text:
+      return "validate_health_form"
+
+  async def required_slots(
+    self,
+    slots_mapped_in_domain: List[Text],
+    dispatcher: "CollectingDispatcher",
+    tracker: "Tracker",
+    domain: "DomainDict"
+  ) -> List[Text]:
+    if tracker.get_slot("confirm_exercise") == True:
+      return ["confirm_exercise", "exercise", "sleep", "diet", "stress", "goal"]
+    else:
+      return ["confirm_exercise", "sleep", "diet", "stress", "goal"]
+
